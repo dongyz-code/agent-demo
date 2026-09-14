9611268a5b9e87508b1589df03896ff6982d8dfb
yuzhong.dong <dongyzchn@gmail.com>
2026-09-14 22:03:46 +0800

    fix: 移除eslint sort

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,22 +1,9 @@
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import perfectionist from 'eslint-plugin-perfectionist';
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  /** perfectionist：仅排序 import，不触碰对象/JSX/类型成员的顺序 */
-  {
-    plugins: { perfectionist },
-    rules: {
-      'perfectionist/sort-imports': ['error', {
-        type: 'natural',
-        order: 'asc',
-        internalPattern: ['^@/.+', '^@repo/.+'],
-        newlinesBetween: 1,
-      }],
-    },
-  },
   /** ------------- */
   {
     rules: {
